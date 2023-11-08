3e94ec40a267250824c07572690470213a7c1561
lmelikishvili <levanimelikishvili@gmail.com>
2023-11-08 17:20:02 +0400

    before_prod

--- app/src/main/java/com/example/homework7/MainActivity.kt
+++ app/src/main/java/com/example/homework7/MainActivity.kt
@@ -3,31 +3,70 @@ package com.example.homework7
 import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.homework7.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val parentLayout = binding.parentLayout
+        val addButton = binding.addButton
+        val chBox = binding.fieldIsNumeric
+        chBox.setOnCheckedChangeListener{
+                buttonView, isChecked ->
+            if (isChecked){
+                binding.edFirstName.inputType = InputType.TYPE_CLASS_NUMBER
+            }else{
+                binding.edFirstName.inputType = InputType.TYPE_CLASS_TEXT
+            }
+        }
 
-        val parentLayout = findViewById<LinearLayout>(R.id.parentLayout)
-        val addButton = findViewById<Button>(R.id.addButton)
+
+
 
         addButton.setOnClickListener {
-            val newEditText = EditText(this)
-            newEditText.setText("New TextView")
-            newEditText.layoutParams = ActionBar.LayoutParams(
+            val firstName = binding.edFirstName.text.toString()
+            val lastName = binding.edLastname.text.toString()
+
+            val newTextview = TextView(this)
+            newTextview.setText("$firstName $lastName")
+            newTextview.layoutParams = ActionBar.LayoutParams(
                 ActionBar.LayoutParams.WRAP_CONTENT,
                 ActionBar.LayoutParams.WRAP_CONTENT,
-
-
             )
+            newTextview.textSize = 30F
+            parentLayout.addView(newTextview)
 
-            parentLayout.addView(newEditText)
+//            if(chBox.isChecked){
+//                val newTextview = TextView(this)
+//                newTextview.setText("$firstName $lastName")
+//                newTextview.layoutParams = ActionBar.LayoutParams(
+//                    ActionBar.LayoutParams.WRAP_CONTENT,
+//                    ActionBar.LayoutParams.WRAP_CONTENT,
+//                )
+//                parentLayout.addView(newTextview)
+//            }else{
+//                val newTextview = TextView(this)
+//                newTextview.setText("$firstName $lastName")
+//                newTextview.layoutParams = ActionBar.LayoutParams(
+//                    ActionBar.LayoutParams.WRAP_CONTENT,
+//                    ActionBar.LayoutParams.WRAP_CONTENT,
+//                )
+//
+//                parentLayout.addView(newTextview)
+//            }
+
+            binding.edFirstName.text.clear()
         }
     }
 }
